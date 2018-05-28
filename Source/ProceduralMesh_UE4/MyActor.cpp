8d7f5d585dd01114d4c41e4e74f80250709949b4
Nick Fox-Gieg <nick@fox-gieg.com>
2018-05-28 03:20:11 -0400

    first test compiles

---
+++ Source/ProceduralMesh_UE4/MyActor.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyActor.h"
+
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	mesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("GeneratedMesh"));
+	RootComponent = mesh;
+	// New in UE 4.17, multi-threaded PhysX cooking.
+	mesh->bUseAsyncCooking = true;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// This is called when actor is spawned (at runtime or when you drop it into the world in editor)
+void AMyActor::PostActorCreated()
+{
+	Super::PostActorCreated();
+	CreateTriangle();
+}
+
+// This is called when actor is already in level and map is opened
+void AMyActor::PostLoad()
+{
+	Super::PostLoad();
+	CreateTriangle();
+}
+
+void AMyActor::CreateTriangle()
+{
+	TArray<FVector> vertices;
+	vertices.Add(FVector(0, 0, 0));
+	vertices.Add(FVector(0, 100, 0));
+	vertices.Add(FVector(0, 0, 100));
+
+	TArray<int32> Triangles;
+	Triangles.Add(0);
+	Triangles.Add(1);
+	Triangles.Add(2);
+
+	TArray<FVector> normals;
+	normals.Add(FVector(1, 0, 0));
+	normals.Add(FVector(1, 0, 0));
+	normals.Add(FVector(1, 0, 0));
+
+	TArray<FVector2D> UV0;
+	UV0.Add(FVector2D(0, 0));
+	UV0.Add(FVector2D(10, 0));
+	UV0.Add(FVector2D(0, 10));
+
+
+	TArray<FProcMeshTangent> tangents;
+	tangents.Add(FProcMeshTangent(0, 1, 0));
+	tangents.Add(FProcMeshTangent(0, 1, 0));
+	tangents.Add(FProcMeshTangent(0, 1, 0));
+
+	TArray<FLinearColor> vertexColors;
+	vertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
+	vertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
+	vertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
+
+	mesh->CreateMeshSection_LinearColor(0, vertices, Triangles, normals, UV0, vertexColors, tangents, true);
+
+	// Enable collision data
+	mesh->ContainsPhysicsTriMeshData(true);
+}
---
+++ Source/ProceduralMesh_UE4/MyActor.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class PROCEDURALMESH_UE4_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		UProceduralMeshComponent * mesh;
+	
+	virtual void PostActorCreated();
+	virtual void PostLoad();
+	virtual void CreateTriangle();
+
+};
